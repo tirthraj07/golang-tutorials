f06134abb30cb6dbe060b0dd4724b843520f29bb
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2026-05-08 23:41:31 +0530

    tut-02: simple values

---
+++ 02-simple-values/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Simple Values
+
+	// Integer
+	fmt.Println(5)
+
+	// String
+	fmt.Println("Hello World")
+
+	// Boolean
+	fmt.Println(true)
+
+	// Floats
+	fmt.Println(10.5)
+
+}
